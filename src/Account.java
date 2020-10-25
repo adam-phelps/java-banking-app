bb8936f409c4e3441a85067a7618e4e13b74a861
adam-phelps <adamkygm@gmail.com>
2020-10-25 14:03:15 -0400

    Functional account class with program run logic.

--- src/Account.java
+++ src/Account.java
@@ -3,18 +3,50 @@ package src;
 import java.util.Scanner;
 
 public class Account {
-    private float accountBalance = 0;
+    private float accountBalance = 1000;
     private String accountOwner = "";
 
-    private static String set_owner()
-    {
+    public String set_owner() {
         Scanner input = new Scanner(System.in);
         System.out.print("Enter in the Account Owner First Name");
         return input.next();
     }
 
-    public Account() {
-        accountOwner = set_owner();
-        System.out.println(accountOwner);
+    public float return_funds() {
+        return accountBalance;
+    }
+
+    public void add_funds(float cashAmount) {
+        accountBalance = accountBalance + cashAmount;
+    }
+
+    public void remove_funds(float cashAmount) {
+        accountBalance = accountBalance - cashAmount;
+    }
+
+    public void user_get_value(float cashAmount, String transactionType) {
+        if (transactionType.equals("Add")) {
+            add_funds(cashAmount);
+        }
+        else if (transactionType.equals("Remove")) {
+            remove_funds(cashAmount);
+        }
+    }
+
+    public void user_start_transaction() {
+        System.out.print("Do you want to (Add) or (Remove) value?");
+        Scanner input = new Scanner(System.in);
+        String userResponse = input.next();
+        System.out.print("How much?");
+        String userTransactionAmount = input.next();
+        float transactionCashAmount = Float.parseFloat(userTransactionAmount);
+        switch (userResponse) {
+            case ("Add"):
+            case ("Remove"):
+                user_get_value(transactionCashAmount, userResponse);
+                break;
+            default:
+                break;
+        }
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,42 @@
 package src;// Adam Phelps
 // Banking Java app
 
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args)
+    private boolean runGame = true;
+    private String userContinueResponse;
+
+    public void set_runGame(boolean runGame) {
+        this.runGame = runGame;
+    }
+
+    public void runGame()
     {
         Account MyAccount = new Account();
+        MyAccount.set_owner();
+        while (runGame == true) {
+            MyAccount.user_start_transaction();
+            System.out.println(MyAccount.return_funds());
+            Scanner input = new Scanner(System.in);
+            System.out.print("Do what you continue adding or removing funds? (Yes/y/No/n)");
+            userContinueResponse = input.next();
+            if (userContinueResponse.equals("Yes") || userContinueResponse.equals("y")) {
+                set_runGame(true);
+            }
+            else if (userContinueResponse.equals("No") || userContinueResponse.equals("n")) {
+                set_runGame(false);
+            }
+            else {
+                System.out.println("Unknown input, aborting.");
+                set_runGame(false);
+            }
+        }
+    }
+
+    public static void main(String[] args)
+    {
+        Main MyMain = new Main();
+        MyMain.runGame();
     }
 }
